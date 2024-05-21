3d517bbf891694da259f1a11b224236de97acbde
safakcavas <safakcvs@gmail.com>
2024-05-21 15:19:01 +0300

    initial commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
 import { useProducts } from "@/hooks/useProducts";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
 
 export default function Index() {
   const { data, error, isLoading } = useProducts();
   return (
-    <View className="items-center justify-center flex-1">
-      <Link href="/products">View user</Link>
-    </View>
+    <SafeAreaView className="items-center justify-center flex-1">
+     <Link href="/register">Go to Register Page </Link>
+     <Link href="/register/login">Go to login Page </Link>
+    </SafeAreaView>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    
   },
   plugins: [],
 };
